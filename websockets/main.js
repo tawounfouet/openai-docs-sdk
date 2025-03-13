eef412b118575eefadd0f990990174a96e2ccb8b
Thomas AWOUNFOUET <78394330+tawounfouet@users.noreply.github.com>
2025-03-13 16:36:30 +0000

    exploring websockets

---
+++ websockets/main.js
@@ -0,0 +1,8 @@
+import { createBoard, playMove } from "./connect4.js";
+
+window.addEventListener("DOMContentLoaded", () => {
+  // Initialize the UI.
+  const board = document.querySelector(".board");
+  createBoard(board);
+});
+
